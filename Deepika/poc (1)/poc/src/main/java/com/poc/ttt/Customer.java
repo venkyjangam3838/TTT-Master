79cb9a613ea4e29e740fe1edc2625fadb2498a37
Deepika20-20 <deepikabiradar20@gmail.com>
2025-02-18 21:49:16 +0530

    deepika poc

---
+++ Deepika/poc (1)/poc/src/main/java/com/poc/ttt/Customer.java
@@ -0,0 +1,69 @@
+package com.poc.ttt;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity      
+public class Customer {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Double height;
+	
+	private Double weight;
+	
+	@OneToOne
+	@JoinColumn(name = "id")
+	private Coach coach;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Coach getCoach() {
+		return coach;
+	}
+
+	public void setCoach(Coach coach) {
+		this.coach = coach;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", height=" + height + ", weight=" + weight + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
